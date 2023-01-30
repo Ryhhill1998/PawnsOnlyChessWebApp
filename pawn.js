95afa0b76e06bae322cd0ddbe9e9cf649e66f640
ryhhill1998 <ryhhill1998@outlook.com>
2023-01-30 16:58:04 +0000

    Finished created base pawn and board classes

--- pawn.js
+++ pawn.js
@@ -1,27 +1,31 @@
 class Pawn {
 
     #colour;
-    #symbol;
+    #imageSrc;
     #xPosition;
     #yPosition;
     #directionMultiplier;
     #hasMoved;
 
-    constructor(colour, symbol, xPosition, yPosition, directionMultiplier) {
+    constructor(colour, xPosition, yPosition, directionMultiplier) {
         this.#colour = colour;
-        this.#symbol = symbol;
+        this.initialiseImageSrc();
         this.#xPosition = xPosition;
         this.#yPosition = yPosition;
         this.#directionMultiplier = directionMultiplier;
         this.#hasMoved = false;
     }
 
-    get colour() {
-        return this.#colour;
+    initialiseImageSrc() {
+        if (this.#colour === "white") {
+            this.#imageSrc = "pieces/white/pawn.svg";
+        } else if (this.#colour === "black") {
+            this.#imageSrc = "pieces/black/pawn.svg";
+        }
     }
 
-    get symbol() {
-        return this.#symbol;
+    get colour() {
+        return this.#colour;
     }
 
     get xPosition() {
@@ -41,6 +45,57 @@ class Pawn {
 
         let coordinates = [];
 
+        if (board.spaceIsFree(this.#xPosition, this.#yPosition + this.#directionMultiplier)) {
+            coordinates[0] = this.#xPosition;
+            coordinates[1] = this.#yPosition + this.#directionMultiplier;
+            possibleMoves.push(coordinates);
+
+            coordinates = [];
+
+            if (!this.#hasMoved
+                && board.spaceIsFree(this.#xPosition, this.#yPosition + (2 * this.#directionMultiplier))) {
+                coordinates[0] = this.#xPosition;
+                coordinates[1] = this.#yPosition + (2 * this.#directionMultiplier);
+                possibleMoves.push(coordinates);
+            }
+        }
+
+        coordinates = [];
+
+        // right take
+        let x = this.#xPosition - this.#directionMultiplier;
+        let y = this.#yPosition + this.#directionMultiplier;
+
+        if (this.#takeIsPossible(board, x, y)) {
+            coordinates[0] = x;
+            coordinates[1] = y;
+            possibleMoves.add(coordinates);
+        }
+
+        // left take
+        x = this.#xPosition + this.#directionMultiplier;
+        y = y = this.#yPosition + this.#directionMultiplier;
+
+        coordinates = [];
+
+        if (this.#takeIsPossible(board, x, y)) {
+            coordinates[0] = x;
+            coordinates[1] = y;
+            possibleMoves.add(coordinates);
+        }
+
         return possibleMoves;
+    }
+
+    #takeIsPossible(board, x, y) {
+        let isPossible = false;
+
+        const pawnAtPosition = board.getPawnAtPosition(x, y);
+
+        if (pawnAtPosition != null && !pawnAtPosition.colour === this.#colour) {
+            isPossible = true;
+        }
+
+        return isPossible;
     }
 }
