e55de247bd94c6216392be6487b58f5bc20103f2
ryhhill1998 <ryhhill1998@outlook.com>
2023-02-09 10:42:36 +0000

    Added ability for user to select 1 player or 2 player and play game

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,24 @@
-import Controller1P from "./controllers/controller1P";
-import Controller2P from "./controllers/controller2P";
-import Model from "./model";
-import View from "./views/view";
+import Controller1P from "./controllers/controller1P.js";
+import Controller2P from "./controllers/controller2P.js";
+import Model from "./model.js";
+import View from "./views/view.js";
 
 const gameModePopUp = document.getElementById("game-mode-pop-up");
 
+const selectGameMode = ({target}) => {
+    const chosenGameMode = target.closest("button")?.id;
+    if (!chosenGameMode) return;
+
+    const model = new Model();
+    const view = new View();
+
+    if (chosenGameMode === "single-player") {
+        new Controller1P(model, view);
+    } else {
+        new Controller2P(model, view);
+    }
+
+    gameModePopUp.classList.add("no-display");
+}
+
+gameModePopUp.addEventListener("click", selectGameMode);
